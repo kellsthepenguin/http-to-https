93f5787f8eec29d82b5505e2acf02812073dfcc7
kells <jwkim101201@gmail.com>
2025-08-15 09:45:06 +0900

    dont log

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ server.on('request', (req, res) => {
   const hostname = req.headers.host.split(':')[0]
   const to = new URL(req.url, `https://${hostname}`).href
 
-  console.log(req.socket.localPort)
-
   if (req.socket.localPort === 80) {
     res.writeHead(301, { location: to })
     res.end()
